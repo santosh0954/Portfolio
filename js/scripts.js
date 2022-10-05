2e7d0de19fcc0dbe2f89fa89a07c7409d397b239
Santosh Jha <santosh0954jha@gmail.com>
2022-10-05 12:37:04 +0530

    animation and favicon added and some other fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
-"dark"===localStorage.theme||!("theme"in localStorage)&&window.matchMedia("(prefers-color-scheme: dark)").matches?document.documentElement.classList.add("dark"):document.documentElement.classList.remove("dark");
+//libraries like jquery etc
+// External JS file like jquery etc which you do not wish to inlcuded in minification
+// //main.js file
+
+// // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+// if (
+//   localStorage.theme === "dark" ||
+//   (!("theme" in localStorage) &&
+//     window.matchMedia("(prefers-color-scheme: dark)").matches)
+// ) {
+//   document.documentElement.classList.add("dark");
+// } else {
+//   document.documentElement.classList.remove("dark");
+// }
+
+// // Whenever the user explicitly chooses light mode
